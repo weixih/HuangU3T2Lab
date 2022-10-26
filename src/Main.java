99b87b98ce786823bc248eb98ed5528f26616c59
student <@bths.edu>
2022-10-26 12:26:48 -0400

    eeee

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]arg){
+        SelectionMadness madness = new SelectionMadness();
+
+        // test flipcoin -- these will return true or false randomly
+        System.out.println(madness.flipCoin());
+        System.out.println(madness.flipCoin());
+        System.out.println(madness.flipCoin());
+
+        // test largest
+        System.out.println(madness.largest(5, 8, 3));
+        System.out.println(madness.largest(8, 5, 3));
+        System.out.println(madness.largest(3, 5, 8));
+        System.out.println(madness.largest(-5, -8, -3));
+        System.out.println(madness.largest(11, 11, 10));
+
+        // test rightTriangle
+        System.out.println(madness.rightTriangle(3, 4, 5));
+        System.out.println(madness.rightTriangle(5, 4, 3));
+        System.out.println(madness.rightTriangle(6, 8, 10));
+        System.out.println(madness.rightTriangle(8, 10, 6));
+        System.out.println(madness.rightTriangle(3, 6, 5));
+        System.out.println(madness.rightTriangle(1, 2, 3));
+
+
+    }
 }
+
--- src/SelectionMadness.java
+++ src/SelectionMadness.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class SelectionMadness {
+public class SelectionMadness
+{
+    // No instance variables //
+
+    // no-parameter constructor with "empty" body since no instance variables
+    public SelectionMadness() { }
+
+    /* Simulates randomly flipping a fair coin and returning true for heads and
+       false for tails; hint: this method should use the Math.random() method to
+       generate a random number.  This method returns true (heads) 50% of the
+       time and false (tails) 50% of the time, on average.
+     */
+    public boolean flipCoin() {
+        int coin = (int)(Math.random()*2)+1;
+        if (coin == 1){
+            return true;
+        }
+        return false;
+    }
+
+
+    /* Returns the largest of three provided integers: num1, num2, or num3
+     */
+    public int largest(int num1, int num2, int num3) {
+        if (num1 > num2){
+            if (num1 > num3){
+                return num1;
+            }
+        }else if(num2 > num3){
+             return num2;
+        }
+        return num3;
+        }
+
+
+    /* Returns true if the three provided lengths create a right triangle, in
+       other words, a2 + b2 = c2, where c is the longest side; returns false
+       Otherwise (hint: use the largest() method that you wrote above!).
+       Be careful -- this is a bit more challenging than it seems!  Consider if
+       side1 = 3, side2 = 4, and side3 = 5 vs. if side1 = 5, side2 = 4, and
+       side3 = 3 -- will your code work regardless of which side is the longest
+       of the three?
+     */
+    public boolean rightTriangle(int side1, int side2, int side3) {
+        int a = 0;
+        int b=0;
+        int c = largest(side1, side2, side3);
+        if (side1 == c){
+            a = side2;
+            b = side3;
+        }
+        if (side2 == c){
+            a=side1;
+            b=side3;
+        }
+        if (side3 ==c){
+            a=side1;
+            b=side2;
+        }
+        if(Math.pow(a, 2) + Math.pow(b,2) == Math.pow(c, 2)){
+            return true;
+        }
+        return false;
+
+    }
+
 }
